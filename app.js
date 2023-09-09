cfc7e03d3007b31e436c0723ffe1f584ffa4dfaa
deepak20431 <deepakubitechsolution98@gmail.com>
2023-09-10 00:03:40 +0530

    add post method

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World rahul')
 })
 
+app.post('/data',async() => {
+  res.send('Hello World rahul')})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
